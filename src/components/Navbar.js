9dc7a73675a0acd122699c6010027722e4a87811
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-21 20:19:19 -0500

    added a new Home component for a home section to the website. Added JS to make navbar collapsible, working on making it fully operational.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react"
+
 function Navbar() {
 
     const navItems = ['Home', 'About/Skills', 'Projects', 'Contact', 'Github/Socials']
@@ -6,8 +8,32 @@ function Navbar() {
         return (<div className="nav-items">{el}</div>)
     })
 
+    //using useState to change visibility of the menu upon being clicked
+    const [hamburger, setHamburger] = useState('hamburger stacked')
+    const [menu, setMenu] = useState('menu hidden')
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    //toggle hamburger menu animation from stacked to crossed
+    function clickedMenu() {
+        if(!isMenuClicked) {
+           setHamburger('hamburger crossed')
+           setMenu('menu visible') 
+        } else {
+            setHamburger('hamburger stacked')
+            setMenu('menu hidden')
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
+
     return (
-        <nav className="navbar">
+        <nav className='navbar'>
+
+            <div className='hamburger-menu' onClick={clickedMenu}>
+                <div className={hamburger} ></div>
+                <div className={hamburger} ></div>
+                <div className={hamburger} ></div>
+            </div>
+
             <span className="name">Varun Malaviya</span>
             {navElements}
         </nav>
