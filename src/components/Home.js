e0b20197f8d6a7f848e1e63ebdd0a0119f3388ee
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-24 20:19:19 -0500

    added a Home component for a short intro to the website. Added styling for that same component.

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+        <div className="home">
+            <div className="intro">
+                <h4>Hello, my name is</h4>
+                <h1>Varun Malaviya</h1>
+                <span>Full Stack Engineer</span>
+                <p>I specialise in creating modern and stylish websites for customers of all sizes.</p>
+                <button>Browse My Portfolio</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
