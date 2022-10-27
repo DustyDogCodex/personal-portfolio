e7ac49345fcaee5f5fe709c53dab1db6ecdda4ce
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-26 20:19:19 -0500

    added a sticky navbar component for easier navigation of the site.

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+function Navbar() {
+
+    const navItems = ['Home', 'About','Portfolio', 'Contact', 'Socials']
+
+    const navElements = navItems.map((item,index) => {
+        return (<div key={index} className="nav-items">{item}</div>)
+    })
+
+    return(
+        <div className="navbar">
+            <div className="nav-name">Varun Malaviya</div>
+            <div className="nav-right">
+                {navElements}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
