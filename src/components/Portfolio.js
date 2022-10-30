3ce0aa6fcb3e5b6f39c88f64ff5f811d5adfcf2e
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-29 20:19:19 -0500

    created a portfolio section. Will add styling later. Has 2 sections. One for showcasing 3 main projects and then a smaller honorable mentions section. These will link to respective projects and repos.

---
+++ src/components/Portfolio.js
@@ -0,0 +1,20 @@
+function Portfolio() {
+    return (
+        <div className="Portfolio">
+            <div className="main-showcase">
+                My most recent projects include:
+                <li>Meme Generator</li>
+                <li>Tenzies Game</li>
+                <li>Airbnb Clone</li>
+            </div>
+            <div className="honorable-mentions">
+                My honorable mentions include:
+                <li>Restaurant Page</li>
+                <li>MMA website</li>
+                <li>Hair salon</li>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio
