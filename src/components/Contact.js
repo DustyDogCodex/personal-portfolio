093ef13e7521d1aef5f462c30e7952a65dec43fb
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-30 20:19:19 -0500

    added a contact section with my current contact information. Will add more if needed.

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+function Contact() {
+    return (
+        <div className="contact">
+            <h3>Contact</h3>
+            <span>Email: varunmalaviya7@gmail.com</span>
+            <a href="https://twitter.com/VarunMalaviya2">Twitter</a>
+            <a href="https://github.com/DustyDogCodex">Github</a>
+        </div>
+    )
+}
+
+export default Contact
