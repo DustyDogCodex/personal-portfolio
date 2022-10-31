8c5816bb87ed541c5de4e8213920069f97bab5d5
Varun Malaviya <varunmalaviya7@gmail.com>
2022-10-30 20:19:19 -0500

    added a footer section. Will work on styling sections that don't have style now.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            Website designed and created by &copy; Varun Malaviya 2023. All rights reserved.
+        </footer>
+    )
+}
+
+export default Footer
