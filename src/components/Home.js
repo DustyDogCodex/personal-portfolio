7d0175d887b9860e8e882eae3f5713dc94df6cf9
Varun Malaviya <varunmalaviya7@gmail.com>
2023-02-22 20:19:19 -0500

    added a fadein animation on screen loading for home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 function Home() {
     return (
         <div className="home">
-            <div className="intro">
-                <h4>Hello, my name is</h4>
-                <h1>Varun Malaviya</h1>
-                <span>Full Stack Engineer</span>
-                <p>I specialise in creating modern and stylish websites for customers of all sizes.</p>
-                <button className="browse-portfolio">Browse My Portfolio</button>
-            </div>
+            <h3 style={{color:'grey'}}>Hi! My name is</h3>
+            <h1>Varun Malaviya</h1>
+            <h5 style={{color:'gold'}}>Full Stack Engineer</h5>
+            <p style={{color:'grey'}}>I specialise in creating modern and stylish websites for customers of all sizes.</p>
+            <button className="browse-portfolio">Browse My Portfolio</button>
         </div>
     )
 }
