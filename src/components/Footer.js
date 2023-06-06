87dc7b25977f4ecadcb5a7bbc3f44b3dccb23f7e
Varun Malaviya <varunmalaviya7@gmail.com>
2023-06-05 21:54:08 -0400

    switched from create-react-app to vite. Importing previouse code next.

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return(
-        <footer>
-            Website designed and created by &copy; Varun Malaviya 2023. All rights reserved.
-        </footer>
-    )
-}
-
-export default Footer
