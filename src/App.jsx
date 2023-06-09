e5d7a5844221c68d12de1eaf9dd355a0db09f709
Varun Malaviya <varunmalaviya7@gmail.com>
2023-06-09 12:04:00 -0400

    fixed export syntax.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
-import Portfolio from './components/Portfolio';
+import { Portfolio } from './components/Portfolio';
 import useMediaQuery from './hooks/useMediaQuery'
 import { SidebarNavDots } from './components/SidebarNavDots';
 
