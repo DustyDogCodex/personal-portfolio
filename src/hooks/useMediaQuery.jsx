38b7ec4b59908f6b33addd4677bfbc70c4308091
vmalaviya <varunmalaviya7@gmail.com>
2023-07-21 13:48:10 -0400

    Added additional comments to better explain code and code logic for useMediaQuery hook. Also cleaned code to improve readability.

--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -2,22 +2,28 @@ import { useState, useEffect } from "react";
 
 const useMediaQuery = (query) => {
     //watching for when screen hits a partiuclar media breakpoint so the website can respond to it.
-    const [matches, setMatches] = useState(false)
+    const [ matches, setMatches ] = useState(false)
 
     useEffect(() => {
-
+        //finding and storing media query object using matchMedia() method in media variable
+        //this will be used to update media queries as user browses through the website
         const media = window.matchMedia(query)
 
-        if( media.matches !== matches){
+        //if current media query is not the same as our state variable matches, we update it 
+        //this is done to grab the media query for the window when the website loads for the first time on a user's browser
+        if( media.matches !== matches ){
             setMatches(media.matches)
         }
 
+        //once the initial media queries for a screen are acquired, any resizing of the window/screen needs to update the current media query.
+        //this is done through the event listeners below
         const listener = () => setMatches(media.matches)
-        
+        //setting matches to cuurent media query when window is resized
         window.addEventListener("resize", listener)
 
+        //removing eventListener because its only needed when window is resized
         return () => window.removeEventListener("resize", listener)
-    }, [matches, query])
+    }, [ matches, query ])
 
     return matches
 }
